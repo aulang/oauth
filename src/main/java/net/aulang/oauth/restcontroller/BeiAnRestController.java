68fb98370f66b969ded2285465b588b6cdfab2e2
Aulang <aulang@qq.com>
2020-05-03 20:01:30 +0800

    feat(view) 添加备案信息查询接口
    添加备案信息查询接口
    
    Aulang
    master

---
+++ src/main/java/net/aulang/oauth/restcontroller/BeiAnRestController.java
@@ -0,0 +1,28 @@
+package net.aulang.oauth.restcontroller;
+
+import net.aulang.oauth.entity.BeiAn;
+import net.aulang.oauth.manage.BeiAnBiz;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Aulang
+ * @email aulang@aq.com
+ * @date 2020-05-03 19:54
+ */
+@RestController
+@RequestMapping("/api/beian")
+public class BeiAnRestController {
+    @Autowired
+    private BeiAnBiz beiAnBiz;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<BeiAn> get() {
+        BeiAn beiAn = beiAnBiz.get();
+        return ResponseEntity.ok(beiAn);
+    }
+}
