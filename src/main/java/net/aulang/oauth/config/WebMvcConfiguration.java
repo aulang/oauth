2f675b3b44955b422cc16ad92d95b009f2fc5ee0
Aulang <aulang@qq.com>
2020-05-09 23:45:22 +0800

    feat(controller) 允许跨域
    允许跨域
    
    Aulang
    master

--- src/main/java/net/aulang/oauth/config/WebMvcConfiguration.java
+++ src/main/java/net/aulang/oauth/config/WebMvcConfiguration.java
@@ -1,9 +1,12 @@
 package net.aulang.oauth.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.WebContentInterceptor;
+
+import static org.springframework.web.cors.CorsConfiguration.ALL;
 
 /**
  * @author Aulang
@@ -17,5 +20,12 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
         WebContentInterceptor contentInterceptor = new WebContentInterceptor();
         contentInterceptor.setCacheSeconds(0);
         registry.addInterceptor(contentInterceptor).addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(ALL)
+                .allowCredentials(true);
     }
 }
