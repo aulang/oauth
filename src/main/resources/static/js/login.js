555c146b01f9a77ec0be85f2fcb2d616606f62d1
Aulang <aulang@qq.com>
2020-09-03 23:15:42 +0800

    feat(SSO): 禁用Cookie无法使用SSO提示
    禁用Cookie无法使用SSO提示
    
    Aulang
    dev

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -2,125 +2,125 @@ $(function () {
     var authorizeId = $('#authorize_id').val();
 
     if (cookieEnabled()) {
-        var username = $('#username');
-        var password = $('#password');
-        var captcha = $('#captcha');
+        $('#errorMsg').html('您的浏览器禁用了cookie，您无法使用单点登录！');
+    }
 
-        var userName = getCookie('USERNAME');
-        if (userName) {
-            username.val(userName);
+    var username = $('#username');
+    var password = $('#password');
+    var captcha = $('#captcha');
+
+    var userName = getCookie('USERNAME');
+    if (userName) {
+        username.val(userName);
+    }
+
+    $('#captchaImg').click(function () {
+        this.src = this.src.split('?')[0] + '?rnd=' + Math.random();
+    });
+
+    $('#loginSubmit').click(function () {
+        if (!$.trim(username.val())) {
+            $('#errorMsg').html('请输入账号');
+            return;
         }
 
-        $('#captchaImg').click(function () {
-            this.src = this.src.split('?')[0] + '?rnd=' + Math.random();
+        var passwordVal = password.val();
+        if (!$.trim(passwordVal)) {
+            $('#errorMsg').html('请输入密码');
+            return;
+        }
+
+        if (captcha.length > 0 && !$.trim(captcha.val())) {
+            $('#errorMsg').html('请输入验证码');
+            return;
+        }
+
+        rememberMe();
+
+        /**
+         * 密码进行SHA256
+         */
+        password.val(sha256(passwordVal));
+
+        $('#loginForm').submit();
+    });
+
+    $('#mobileLoginSubmit').click(function () {
+        var mobile = $('#mobile').val();
+        var mobileCaptcha = $('#mobileCaptcha').val();
+        if (!mobile) {
+            $("#errorMsg").html("请输入手机号码");
+            return;
+        }
+        if (!sentCaptcha) {
+            $("#errorMsg").html("请先发送验证码");
+            return;
+        }
+        if (!mobileCaptcha) {
+            $("#errorMsg").html("请输入验证码");
+            return;
+        }
+
+        var url = $(this).attr("href") + mobileCaptcha;
+
+        $.ajax({
+            type: "get",
+            url: url,
+            dataType: "text"
+        }).done(function () {
+            $('#mobileLoginForm').submit();
+        }).fail(function () {
+            $("#errorMsg").html("验证码错误");
         });
+    });
 
-        $('#loginSubmit').click(function () {
-            if (!$.trim(username.val())) {
-                $('#errorMsg').html('请输入账号');
-                return;
-            }
+    var sentCaptcha = false;
+    $('#sendCaptcha').click(function () {
+        var $this = $(this);
+        var url = $this.attr("href");
 
-            var passwordVal = password.val();
-            if (!$.trim(passwordVal)) {
-                $('#errorMsg').html('请输入密码');
-                return;
-            }
+        var mobile = $("#mobile").val();
+        if (!mobile || !/^1[\d]{10}$/.test(mobile)) {
+            $("#errorMsg").html("请输入手机号码");
+            return;
+        }
 
-            if (captcha.length > 0 && !$.trim(captcha.val())) {
-                $('#errorMsg').html('请输入验证码');
-                return;
-            }
-
-            rememberMe();
-
+        $this.attr("disabled", true);
+        $.ajax({
+            type: "post",
+            url: url,
+            data: {authorize_id: authorizeId, mobile: mobile},
+            dataType: "json"
+        }).done(function (result) {
             /**
-             * 密码进行SHA256
+             * result.target
+             * result.requestId
              */
-            password.val(sha256(passwordVal));
+            sentCaptcha = true;
+            $("#errorMsg").html("已向您的" + result.target + "发送验证码");
 
-            $('#loginForm').submit();
+            var seconds = 60;
+            var timer = setInterval(function () {
+                --seconds;
+                $this.html(seconds + 'S');
+                if (seconds < 1) {
+                    $this.attr("disabled", false);
+                    $this.html('发送验证码');
+                    clearInterval(timer);
+                    timer = null;
+                }
+            }, 1000);
+        }).fail(function (res) {
+            sentCaptcha = false;
+            $this.attr("disabled", false);
+            $("#errorMsg").html(res.responseText);
         });
+    });
 
-        $('#mobileLoginSubmit').click(function () {
-            var mobile = $('#mobile').val();
-            var mobileCaptcha = $('#mobileCaptcha').val();
-            if (!mobile) {
-                $("#errorMsg").html("请输入手机号码");
-                return;
-            }
-            if (!sentCaptcha) {
-                $("#errorMsg").html("请先发送验证码");
-                return;
-            }
-            if (!mobileCaptcha) {
-                $("#errorMsg").html("请输入验证码");
-                return;
-            }
-
-            var url = $(this).attr("href") + mobileCaptcha;
-
-            $.ajax({
-                type: "get",
-                url: url,
-                dataType: "text"
-            }).done(function () {
-                $('#mobileLoginForm').submit();
-            }).fail(function () {
-                $("#errorMsg").html("验证码错误");
-            });
-        });
-
-        var sentCaptcha = false;
-        $('#sendCaptcha').click(function () {
-            var $this = $(this);
-            var url = $this.attr("href");
-
-            var mobile = $("#mobile").val();
-            if (!mobile || !/^1[\d]{10}$/.test(mobile)) {
-                $("#errorMsg").html("请输入手机号码");
-                return;
-            }
-
-            $this.attr("disabled", true);
-            $.ajax({
-                type: "post",
-                url: url,
-                data: {authorize_id: authorizeId, mobile: mobile},
-                dataType: "json"
-            }).done(function (result) {
-                /**
-                 * result.target
-                 * result.requestId
-                 */
-                sentCaptcha = true;
-                $("#errorMsg").html("已向您的" + result.target + "发送验证码");
-
-                var seconds = 60;
-                var timer = setInterval(function () {
-                    --seconds;
-                    $this.html(seconds + 'S');
-                    if (seconds < 1) {
-                        $this.attr("disabled", false);
-                        $this.html('发送验证码');
-                        clearInterval(timer);
-                        timer = null;
-                    }
-                }, 1000);
-            }).fail(function (res) {
-                sentCaptcha = false;
-                $this.attr("disabled", false);
-                $("#errorMsg").html(res.responseText);
-            });
-        });
-
-        $('#loginModeSwitch').click(function () {
-            $('#loginForm').toggle();
-            $('#mobileLoginForm').toggle();
-        });
-    } else {
-        $('#errorMsg').html('您的浏览器禁用了cookie，您无法正常登录！');
-    }
+    $('#loginModeSwitch').click(function () {
+        $('#loginForm').toggle();
+        $('#mobileLoginForm').toggle();
+    });
 });
 
 function rememberMe() {
