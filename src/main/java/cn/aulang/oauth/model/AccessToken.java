43d3668de5f3e79c3dcf88b40a9208d337b4d5a4
wulang <wulang@unicloud.com>
2022-11-03 17:31:23 +0800

    符合标准些

--- src/main/java/cn/aulang/oauth/model/AccessToken.java
+++ src/main/java/cn/aulang/oauth/model/AccessToken.java
@@ -1,5 +1,6 @@
 package cn.aulang.oauth.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,8 +15,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AccessToken {
 
+    @JsonProperty("access_token")
     private String accessToken;
+    @JsonProperty("refresh_token")
     private String refreshToken;
+    @JsonProperty("expires_in")
     private int expiresIn;
 
     public static AccessToken create(String accessToken, String refreshToken, int expiresIn) {
